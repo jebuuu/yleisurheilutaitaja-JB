b3385a1014884629c1f20573e9832f7dd96bba53
Juho Borén <juho.boren@student.jedu.fi>
2017-05-18 11:44:52 +0300

    db

--- index.php
+++ index.php
@@ -16,18 +16,30 @@ foreach ($_SERVER as $key => $value) {
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-define('DB_NAME', 'yleisurheilutaitaja');
+define('DB_NAME', $connectstr_dbname);
 
 /** MySQL database username */
-define('DB_USER', 'azure');
+define('DB_USER', $connectstr_dbusername);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', $connectstr_dbpassword);
 
 /** MySQL hostname : this contains the port number in this format host:port . Port is not 3306 when using this feature*/
-define('DB_HOST', 'localhost');
+define('DB_HOST', $connectstr_dbhost);
 
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
 
 session_start();
 if(isset($_GET['sivu'])){
